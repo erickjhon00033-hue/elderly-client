0064385ed5e87a5fdfa2b461f96d52269fee71a1
Erick Reyes <erickjhon000033@gmail.com>
2026-01-10 17:49:24 -0400

    bien

--- src/ProductDetail.jsx
+++ src/ProductDetail.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { useParams } from "react-router-dom";
-import { products } from "./products"; // ✅ Importamos los datos correctamente
 
-function ProductDetail({ addToCart }) {
+function ProductDetail({ products, addToCart }) {
   const { id } = useParams();
   const product = products.find((p) => p.id === parseInt(id));
 
@@ -71,7 +70,7 @@ function ProductDetail({ addToCart }) {
 
       {/* Información del producto */}
       <h2>{product.name}</h2>
-      <p className="price">Precio: RD${product.price}</p>
+      <p className="price">Precio: RD${product.price.toFixed(2)}</p>
       <p>{product.description}</p>
 
       {/* Secciones expandibles */}
