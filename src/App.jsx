00d7960cd9c91c3d22c17f1566d68286450d7db7
Erick Reyes <erickjhon000033@gmail.com>
2026-01-10 10:49:38 -0400

    change logo name

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import logo from './assets/logo.png';
+import logo from './assets/Logo.png';
 import Products from './Products';
 import Cart from './Cart';
 import Checkout from './Checkout';
